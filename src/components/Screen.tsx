06278059ac012c59cebb0c25378330645d483318
Tim Lai <timlaicodes@gmail.com>
2026-05-23 20:01:53 -0400

    Created Screen component

---
+++ src/components/Screen.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import type { Mesh } from "three";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+type TableProps = {
+  position?: [number, number, number];
+  rotation?: [number, number, number];
+  scale?: number;
+};
+
+export default function Screen({
+  position = [7.5, -0.4, -10],
+  rotation = [0, 1.6, 0],
+  scale = 2,
+}: TableProps) {
+  const gltf = useLoader(GLTFLoader, "/models/screen.gltf");
+  const clone = gltf.scene.clone(true);
+
+  useEffect(() => {
+    if (!clone) return;
+
+    clone.traverse((object) => {
+      if ((object as Mesh).isMesh) {
+        const mesh = object as Mesh;
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+      }
+    });
+  }, [clone]);
+
+  return (
+    <primitive
+      object={clone}
+      position={position}
+      rotation={rotation}
+      scale={scale}
+    />
+  );
+}
