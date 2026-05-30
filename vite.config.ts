f85a71854b8a5e3267f236bd850fe1866cf7011b
Tim Lai <timlaicodes@gmail.com>
2026-05-30 17:03:23 -0400

    Added plugin to copy _headers to dist to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   // server: { host: true, https: false, strictPort: true, allowedHosts: true },
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    basicSsl(),
+    {
+      name: "copy-headers",
+      closeBundle() {
+        try {
+          fs.copyFileSync("_headers", "dist/_headers");
+        } catch (e) {
+          console.error("Failed to copy _headers:", e);
+        }
+      },
+    },
+  ],
   base: "/",
   build: {
     assetsDir: "",
