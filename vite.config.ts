6bcc78ba5d9bf58981e8ac2238beec807aa6d980
Tim Lai <timlaicodes@gmail.com>
2026-05-30 11:56:06 -0400

    Removed ViteImageOptimizer and viteCompression

--- vite.config.ts
+++ vite.config.ts
@@ -1,77 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
-import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
   // server: { host: true, https: false, strictPort: true, allowedHosts: true },
-  plugins: [
-    react(),
-    basicSsl(),
-    ViteImageOptimizer({
-      test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
-      exclude: undefined,
-      include: undefined,
-      includePublic: true,
-      logStats: true,
-      ansiColors: true,
-      svg: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                cleanupNumericValues: false,
-                cleanupIds: {
-                  minify: false,
-                  remove: false,
-                },
-                convertPathData: false,
-              },
-            },
-          },
-          "sortAttrs",
-          {
-            name: "addAttributesToSVGElement",
-            params: {
-              attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
-            },
-          },
-        ],
-      },
-      png: {
-        // https://sharp.pixelplumbing.com/api-output#png
-        quality: 100,
-      },
-      jpeg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
-        quality: 100,
-      },
-      jpg: {
-        // https://sharp.pixelplumbing.com/api-output#jpeg
-        quality: 100,
-      },
-      tiff: {
-        // https://sharp.pixelplumbing.com/api-output#tiff
-        quality: 100,
-      },
-      // gif does not support lossless compression
-      // https://sharp.pixelplumbing.com/api-output#gif
-      gif: {},
-      webp: {
-        // https://sharp.pixelplumbing.com/api-output#webp
-        lossless: true,
-      },
-      avif: {
-        // https://sharp.pixelplumbing.com/api-output#avif
-        lossless: true,
-      },
-      cache: false,
-      cacheLocation: undefined,
-    }),
-    viteCompression(),
-  ],
+  plugins: [react(), basicSsl()],
 });
