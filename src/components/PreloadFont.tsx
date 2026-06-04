707be9212ddfc5155f0275890a7310c22eafa77c
Tim Lai <timlaicodes@gmail.com>
2026-06-04 11:00:18 -0400

    Replaced ttf with msdf font

--- src/components/PreloadFont.tsx
+++ src/components/PreloadFont.tsx
@@ -1,14 +1,18 @@
-import { useTTF, type FontFamilies } from "@react-three/uikit";
+import { type FontFamilies } from "@react-three/uikit";
 
 const specialGothicCondensed =
-  "/fonts/SpecialGothicCondensedOne_Regular_subset.ttf";
+  "/fonts/SpecialGothicCondensedOne_Regular_subset_msdf.json";
 
 type PreloadFontProps = {
   setFontFamilies: (fontFamilies: FontFamilies) => void;
 };
 
 export default function PreloadFont({ setFontFamilies }: PreloadFontProps) {
-  const fontFamilies = useTTF(specialGothicCondensed);
+  const fontFamilies = {
+    gothic: {
+      regular: specialGothicCondensed,
+    },
+  };
 
   if (fontFamilies) {
     console.log("Font Families:", fontFamilies);
